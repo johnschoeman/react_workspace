acfc2bf1ca23159d2cf199e366ebef988a839a10
John Schoeman <johnschoeman1617@gmail.com>
2018-12-28 12:08:37 -0800

    Complete 02: pass data with a callback

--- react_context_egghead/src/LoginPage.js
+++ react_context_egghead/src/LoginPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import UserContext from './UserContext';
 import {login} from './api';
 
 class LoginPage extends React.Component {
@@ -15,13 +17,13 @@ class LoginPage extends React.Component {
     });
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e, onLogin) => {
     e.preventDefault();
     this.setState({loading: true, error: null});
     login(this.state.username, this.state.password)
       .then(user => {
         this.setState({loading: false});
-        this.props.onLogin(user);
+        onLogin(user);
       })
       .catch(error => this.setState({error, loading: false}));
   };
@@ -30,31 +32,35 @@ class LoginPage extends React.Component {
     const {username, password, error, loading} = this.state;
 
     return (
-      <div className="LoginPage">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Username
-            <input
-              name="username"
-              value={username}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <label>
-            Password
-            <input
-              name="password"
-              type="password"
-              value={password}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          {error && <div className="error">{error.message}</div>}
-          <button type="submit" disabled={loading}>
-            Sign In
-          </button>
-        </form>
-      </div>
+      <UserContext.Consumer>
+        {({onLogin}) => (
+          <div className="LoginPage">
+            <form onSubmit={e => this.handleSubmit(e, onLogin)}>
+              <label>
+                Username
+                <input
+                  name="username"
+                  value={username}
+                  onChange={this.handleInputChange}
+                />
+              </label>
+              <label>
+                Password
+                <input
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={this.handleInputChange}
+                />
+              </label>
+              {error && <div className="error">{error.message}</div>}
+              <button type="submit" disabled={loading}>
+                Sign In
+              </button>
+            </form>
+          </div>
+        )}
+      </UserContext.Consumer>
     );
   }
 }
